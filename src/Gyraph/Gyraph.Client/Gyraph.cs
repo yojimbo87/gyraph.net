8fd516a3ff533aa3e027e4095ec780a3a7573e42
Tomas Bosak <bosak.tomas@gmail.com>
2013-05-31 21:59:57 +0200

    Create basic project structure.

---
+++ src/Gyraph/Gyraph.Client/Gyraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Gyraph.Client
+{
+    public class Gyraph : IDisposable
+    {
+        private RedisClient _redisClient;
+
+        public Gyraph()
+        {
+            _redisClient = GyraphManager.GetClient();
+        }
+
+        public void Dispose()
+        {
+            GyraphManager.DisposeClient(_redisClient);
+            _redisClient = null;
+        }
+    }
+}
---
+++ src/Gyraph/Gyraph.Client/GyraphManager.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack.Redis;
+
+namespace Gyraph.Client
+{
+    public static class GyraphManager
+    {
+        private static PooledRedisClientManager _redis;
+
+        public static void Connection(string host, int port, string password, int database)
+        {
+            _redis = new PooledRedisClientManager(database, string.Format("{0}@{1}:{2}", password, host, port));
+        }
+
+        internal static RedisClient GetClient()
+        {
+            return (RedisClient)_redis.GetClient();
+        }
+
+        internal static void DisposeClient(RedisClient client)
+        {
+            _redis.DisposeClient(client);
+        }
+    }
+}
